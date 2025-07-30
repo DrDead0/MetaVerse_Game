6a952a5426c41040f532ece7f298086e03cc692e
DrDead0 <112349103+DrDead0@users.noreply.github.com>
2025-07-30 06:11:52 +0530

    Debugged types for ts

--- metaverse/apps/http/src/index.ts
+++ metaverse/apps/http/src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { router } from './Routers/v1/index.router';
+import { router } from './Routers/v1/index.router.js';
 const app = express();
 app.use("/api/v1", router); // this is prefix router and can be used in many ways and many times
 //instead of using app.get or app.post number of times
